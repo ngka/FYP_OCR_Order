946275383824a8e1e8edbc5f2c713d81170800c5
ngka <45294674+ngka@users.noreply.github.com>
2023-07-26 20:51:00 +0800

    [ADD] Query_Ng(Java&XML)

---
+++ FYP_OCR_Order/app/src/main/java/com/example/fyp_ocr_order/Query_Ng.java
@@ -0,0 +1,53 @@
+package com.example.fyp_ocr_order;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class Query_Ng extends AppCompatActivity {
+    TextView textView;
+    ImageButton button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_query_ng);
+
+        button = findViewById(R.id.submit);
+        textView = findViewById(R.id.textView);
+        EditText edit_Username = findViewById(R.id.Username);
+        EditText edit_Email = findViewById(R.id.Email);
+
+        button.setOnClickListener(view -> {
+            String Username = edit_Username.getText().toString();
+            String Email = edit_Email.getText().toString();
+            RequestQueue queue = Volley.newRequestQueue(this);
+            String url = "http://192.168.56.49/FYP/FYP_websiteData/user/get_data.php?Username="+ Username + "&Email=" + Email;
+            StringRequest myReq = new StringRequest(Request.Method.GET, url,
+                    response -> {
+                        if (!response.equals("No data found")) {
+                            String question = response.trim();
+                            textView.setText(question);
+                        } else {
+                            Toast.makeText(this, "No data found", Toast.LENGTH_SHORT).show();
+                        }
+                    },
+                    error -> {
+                        Log.e("Error", error.getLocalizedMessage());
+                    });
+            queue.add(myReq);
+        });
+    }
+}
+
